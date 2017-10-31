6b36a6a1811b69701d8244a47c934253fc9d1c8d
David Amado <david.amado9411@gmail.com>
2017-10-30 20:29:34 -0500

    falta mandar procesos a la cola de espera y suspendido

---
+++ procesos.py
@@ -0,0 +1,35 @@
+import threading
+import time
+import numpy as np
+class Proceso:
+    def __init__(self,idProceso,quantum,nombre,recurso,t):
+        self.idProceso=idProceso
+        self.nombre=nombre
+        self.recurso=recurso
+        self.t=t
+        self.quantum=quantum
+        self.terminado=False
+    def __str__(self):
+        return self.nombre+" "+str(self.idProceso)
+    def procesar(self):
+        while self.quantum and self.t:
+            
+            print("Preparando ",self.nombre," ",self.idProceso," quantum ",self.quantum,"t",self.t)
+            self.quantum-=1
+            self.t-=1
+            time.sleep(1)
+    def asignarQ(self):
+        return np.random.randint(5,15) 
+
+class PolloConPapas(Proceso):
+    def __init__(self,idProceso,quantum=0,nombre="Pollo con papas",recurso="Horno",t=25):
+        Proceso.__init__(self,idProceso,quantum,nombre,recurso,t)
+
+class Malteada(Proceso):
+    def __init__(self,idProceso,quantum=0,nombre="Malteada",recurso="Licuadora",t=5):
+        Proceso.__init__(self,idProceso,quantum,nombre,recurso,t)
+
+class Ensalada(Proceso):
+    def __init__(self,idProceso,quantum=0,nombre="Ensalada",recurso="Cuchillo",t=12):
+        Proceso.__init__(self,idProceso,quantum,nombre,recurso,t)
+
