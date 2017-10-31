2de026ff86843562acdb9d07c991fc5e36041e2a
David Amado <david.amado9411@gmail.com>
2017-10-31 17:24:40 -0500

    Round Robin con estadisticas del proceso para diagrama de gantt

--- procesos.py
+++ procesos.py
@@ -8,17 +8,21 @@ class Proceso:
         self.t=t
         self.tr=tr
         self.quantum=quantum
-      
+        self.sus=0
+        self.blo=0
+        self.lis=0
+        self.zc=0
     def __str__(self):
         return self.nombre+" "+str(self.idProceso)
     def procesar(self):
         self.quantum-=1
         self.t-=1
+        self.zc+=1
         print("Preparando",self.nombre,self.idProceso,"quantum",self.quantum,"t",self.t,"recurso",self.recurso)
             
     def asignarQ(self): 
-        if self.t>10:
-            return (round(self.t*0.6)) 
+        if self.t>6:
+            return(round(self.t*0.6)) 
         else:
             return self.t
 
@@ -28,10 +32,10 @@ class PolloConPapas(Proceso):
         Proceso.__init__(self,idProceso,quantum,nombre,recurso,t,tr)
 
 class Malteada(Proceso):
-    def __init__(self,idProceso,recurso,quantum=0,nombre="Malteada",t=5,tr=0):
+    def __init__(self,idProceso,recurso,quantum=0,nombre="Malteada",t=10,tr=0):
         Proceso.__init__(self,idProceso,quantum,nombre,recurso,t,tr)
 
 class Ensalada(Proceso):
-    def __init__(self,idProceso,recurso,quantum=0,nombre="Ensalada",t=12,tr=0):
+    def __init__(self,idProceso,recurso,quantum=0,nombre="Ensalada",t=15,tr=0):
         Proceso.__init__(self,idProceso,quantum,nombre,recurso,t,tr)
 
--- roundRobin.py
+++ roundRobin.py
@@ -5,7 +5,7 @@ import recursos as rs
 import queue
 import threading
 class Procesador(threading.Thread):
-    def __init__(self,idProcesador,r,*args):
+    def __init__(self,idProcesador,*args):
         threading.Thread.__init__(self)
         self.idProcesador=idProcesador 
         self.proceso=None
@@ -13,22 +13,17 @@ class Procesador(threading.Thread):
         self.blo=cola.Cola()
         self.sus=cola.Cola()
         self._args=args
-        self._r=r
         self.uso=True
     def __str__(self):
         return str(self.idProcesador)
     def run(self):
         while self.uso:
-            self.usarProcesador(*self._args,self._r)
-    def usarProcesador(self,q,r):
+            self.usarProcesador(*self._args)
+    def usarProcesador(self,q):
         while not self.proceso==None or not q.empty() or not self.lis.es_vacia() or not self.sus.es_vacia() or not self.blo.es_vacia():
             
             time.sleep(0.1)
-            #for i in r:
-             #   print("recurso",i,"estado",i.libre)
-
-            #print("lis",self.lis.items,"procesador",self,"\nsus",self.sus.items,"procesador",self,"\nblo",self.blo.items,"procesador",self)
-            #print("procesador",self,"ocupado",not self.proceso==None)
+            
             if not q.empty():self.asignar(q.get())
             if not self.lis.es_vacia() and self.proceso==None:
                 posible=self.lis.desencolar()
@@ -41,7 +36,8 @@ class Procesador(threading.Thread):
                     print("\nel proceso",posible,"requiere de un recurso ocupado, encolando en bloqueado")
                     self.blo.encolar(posible)
             self.revisarColaSus()
-            self.revisarColaBlo() 
+            self.revisarColaBlo()
+            self.contarColaLis()
             if not self.proceso==None:
                 self.proceso.procesar()
                 if self.proceso.t>0 and self.proceso.quantum==0:
@@ -52,7 +48,7 @@ class Procesador(threading.Thread):
                     self.proceso=None
                 elif self.proceso.t==0:
                     self.proceso.recurso.libre=True                    
-                    print("\nterminando proceso",self.proceso,"en el procesador",self)
+                    print("\nterminando proceso",self.proceso,"en el procesador",self,",sus",self.proceso.sus,",lis",self.proceso.lis,",blo",self.proceso.blo,",zona critica",self.proceso.zc)
                     self.proceso=None
                     q.task_done()
         print("termino el procesador",self)
@@ -60,20 +56,26 @@ class Procesador(threading.Thread):
     def revisarColaSus(self):
         for i in range(len(self.sus.items)):
             self.sus.items[i].tr-=1
+            self.sus.items[i].sus+=1
         if not self.sus.es_vacia():
             print(self.sus.items[0],"tr",self.sus.items[0].tr)
             if self.sus.items[0].tr==0:
                 a=self.sus.desencolar()
                 self.asignar(a)
-                print("se saco el proceso",a,"de la cola de suspendidosy entro a la cola de listo")
+                print("\nse saco el proceso",a,"de la cola de suspendidos y entro a la cola de listo")
     def revisarColaBlo(self):
-        for i in range(len(self.blo.items)):
-            if self.blo.items[i].recurso.libre:
-                a=self.blo.desencolar(i)
+        
+        for i in self.blo.items:
+            if i.recurso.libre:
+                i.blo+=1
+                a=self.blo.desencolar(self.blo.items.index(i))
                 self.asignar(a)
-                self.revisarColaBlo()
-                break
-                print("\nsaco el proceso",a," de la cola de bloqueados!!!!!!!!!!!!!!!!!!!!!")
+                print("\nse saco el proceso",a," de la cola de bloqueados y entro en la cola de listos")
+
+    def contarColaLis(self):
+        for i in self.lis.items:
+            i.lis+=1
+
     def asignar(self,proceso):
         proceso.quantum=proceso.asignarQ()
         self.lis.encolar(proceso)
@@ -88,9 +90,9 @@ class cliente:
         self.cola3=queue.Queue()
         self.cola3.put(ps.Ensalada(0,self.recursos[1]))
         
-        self.procesador1=Procesador(1,self.recursos,self.cola1)        
-        self.procesador2=Procesador(2,self.recursos,self.cola2)      
-        self.procesador3=Procesador(3,self.recursos,self.cola3)
+        self.procesador1=Procesador(1,self.cola1)        
+        self.procesador2=Procesador(2,self.cola2)      
+        self.procesador3=Procesador(3,self.cola3)
     def iniciar(self):
         self.procesador1.start()
         self.procesador2.start()
